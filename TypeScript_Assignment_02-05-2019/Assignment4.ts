b184f7bf97726ccfe928b20da75ce298e8bb96aa
Divya9911 <50020317+Divya9911@users.noreply.github.com>
2019-05-02 20:56:15 +0530

    Search input in the tuple

---
+++ TypeScript_Assignment_02-05-2019/Assignment4.ts
@@ -0,0 +1,17 @@
+var tuple = {
+	"user_1": "Riya",
+	"user_2": "Pooja",
+	"user_3": "Jyoti"
+}
+
+function searchInTuple(val: string): string	{
+	for(var key in tuple)	{
+		if(key == val)	{
+			return `${key}: ${tuple[key]}`;
+		}
+	}
+	return "Nothing is found!";
+}
+
+console.log(searchInTuple("user_1"));
+console.log(searchInTuple("user_6"));
