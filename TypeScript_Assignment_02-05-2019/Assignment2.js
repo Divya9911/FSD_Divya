bd9acfd6af1f801a853b62987ee42c640a6437cc
Divya9911 <50020317+Divya9911@users.noreply.github.com>
2019-05-02 20:45:48 +0530

    JS File for sum of first 100 no's

--- TypeScript_Assignment_02-05-2019/Assignment2.js
+++ TypeScript_Assignment_02-05-2019/Assignment2.js
@@ -1,14 +1,15 @@
-import * as readline from 'readline'
-let valFromRuntime = readline.createInterface({
+"use strict";
+exports.__esModule = true;
+var readline = require("readline");
+var valFromRuntime = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-})
-
-valFromRuntime.question('Please enter a number',(answer) =>{
-    var n = parseInt(answer);//converting string into integer
+});
+valFromRuntime.question('Please enter a number', function (answer) {
+    var n = parseInt(answer); //converting string into integer
     var sum = 0;
-    for(var i=n ;i<n+100 ;i++){
-    sum = sum + i;
+    for (var i = n; i < n + 100; i++) {
+        sum = sum + i;
     }
     console.log(sum);
     valFromRuntime.close();
