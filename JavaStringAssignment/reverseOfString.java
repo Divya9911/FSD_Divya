4c8a322896ccd8e5282ceabdaddc183140ebd64b
Divya9911 <50020317+Divya9911@users.noreply.github.com>
2019-05-12 19:29:27 +0530

    Add files via upload

---
+++ JavaStringAssignment/reverseOfString.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class reverseOfString{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		String str = sc.nextLine();
+	    String reverse ="";
+		int i;
+		for(i= str.length()-1; i>=0;i--)
+		{
+			reverse = reverse + str.charAt(i); 
+		}
+		System.out.println(reverse);
+		
+
+	}
+}
