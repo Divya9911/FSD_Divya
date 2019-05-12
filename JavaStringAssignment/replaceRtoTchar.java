db8ff7ccd873a3da810422cb51b6d93a53e23d00
Divya9911 <50020317+Divya9911@users.noreply.github.com>
2019-05-12 19:28:41 +0530

    Add files via upload

---
+++ JavaStringAssignment/replaceRtoTchar.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class replaceRtoTchar{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the string");
+		String str = sc.nextLine();
+		System.out.println(str.replace('r','t'));
+	}}
