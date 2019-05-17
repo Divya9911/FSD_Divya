380bdec6396ac36afc4970dfbaec6603a75e2fef
Divya9911 <50020317+Divya9911@users.noreply.github.com>
2019-05-17 17:34:41 +0530

    Add files via upload

---
+++ Java So Far 17-May-2019/CalculateDifference.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class CalculateDifference{
+	public static void main(String[] args) {
+		CalculateDifference difference = new CalculateDifference();
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n =sc.nextInt();
+		System.out.println("Difference is: "+difference.calculateDifference(n));
+	}
+	int calculateDifference(int n){
+		int diff;
+		int sumWholeSquare=0;
+		int sumSquareOfDigit=0;
+		for(int i=1 ;i<=n;i++){
+			sumSquareOfDigit = sumSquareOfDigit + i*i; 
+			sumWholeSquare =sumWholeSquare + i;
+		}
+		diff = (sumWholeSquare*sumWholeSquare)-sumSquareOfDigit;
+		return diff; 
+	}
+}
