2c346d7e3af6a3fc0d8d6cdf4f830a2396e8550c
Divya9911 <50020317+Divya9911@users.noreply.github.com>
2019-05-20 17:03:21 +0530

    Update EmployeeServiceInterface.java

--- Layered Architecture 20-May-2019/FirstJavaApplication/src/com/ibm/eis/service/EmployeeServiceInterface.java
+++ Layered Architecture 20-May-2019/FirstJavaApplication/src/com/ibm/eis/service/EmployeeServiceInterface.java
@@ -4,21 +4,12 @@ import java.util.Map;
 
 import com.ibm.eis.bean.Employee;
 
-public interface EmployeeServiceInterface {
-	//void findInsuaranceScheme(Employee emp1);
+public interface EmployeeServiceInterface{
 	String EmpDetails();
-	//void storeIntoMap(Employee emp);
+	
 	void findInsuaraceScheme(Employee emp);
 	void storeIntoMap(int id, Employee emp);
 	boolean checkId(int id);
-	//void storeIntoMap(Employee emp);
-
-	//void findInsuaranceScheme(Employee emp1);
-
-	//void storeIntoMap(Employee emp1,Employee emp2, Employee emp3);
-
-	//void findInsuaraceScheme(Employee emp1);
-
 	
 	
 
